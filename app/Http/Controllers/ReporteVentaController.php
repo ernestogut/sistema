010730ac8bccd5c25e962994a9bda4d5acb3baa7
Ernesto Gutierrez <ernestogutttt@gmail.com>
2020-04-21 02:03:49 -0500

    intento6heroku

--- app/Http/Controllers/ReporteVentaController.php
+++ app/Http/Controllers/ReporteVentaController.php
@@ -16,7 +16,7 @@ class ReporteVentaController extends Controller
      */
     public function index()
     {
-        $ventas = ReporteVenta::select(DB::raw('YEAR(fecha) as year, MONTHNAME(fecha) month, SUM(total) as subtotal, count(*) as orders'))->groupBy(DB::raw('YEAR(fecha), MONTHNAME(fecha)'))->orderBy(DB::raw('MONTH(fecha)'))->where('id_almacen', '=', 2)->get();
+        $ventas = ReporteVenta::select(DB::raw('to_char(fecha, "YYYY") as year, to_char(fecha, "Month") as month, SUM(total) as subtotal, count(*) as orders'))->groupBy(DB::raw('to_char(fecha, "YYYY"), to_char(fecha, "Month")'))->orderBy(DB::raw('to_char(fecha, "MM")'))->where('id_almacen', '=', 2)->get();
         return $ventas;
     }
 
@@ -50,7 +50,7 @@ class ReporteVentaController extends Controller
     public function show($almacen)
     {
         //DB::statement("SET lc_time_names = 'es_ES'");
-        $ventas = ReporteVenta::select(DB::raw('YEAR(fecha) as year, MONTHNAME(fecha) month, SUM(total) as total, count(*) as ordenes'))->groupBy(DB::raw('YEAR(fecha), MONTHNAME(fecha)'))->orderBy(DB::raw('MONTH(fecha)'))->where('id_almacen', '=', $almacen)->get();
+        $ventas = ReporteVenta::select(DB::raw('to_char(fecha, "YYYY") as year, to_char(fecha, "Month") as month;, SUM(total) as total, count(*) as ordenes'))->groupBy(DB::raw('to_char(fecha, "YYYY"), to_char(fecha, "Month")'))->orderBy(DB::raw('to_char(fecha, "MM")'))->where('id_almacen', '=', $almacen)->get();
         return $ventas;
     }
 
